f4cd94aeb9a858c16cb3f91508cde10a7cd3eb4e
NITS <nits@Nits-MacBook-Air.local>
2024-11-26 19:39:19 -0500

    Added Q&A Page and updated the UI

--- routes/qna.js
+++ routes/qna.js
@@ -5,4 +5,10 @@ router.route("/").get(async (req, res) => {
   return res.status(200).render("qna");
 });
 
+router.route("/ans/:courseCode").get(async (req, res) => {
+  let courseCode = req.params.courseCode;
+  console.log(courseCode);
+  return res.status(200).render("qnaCourseAnswers", { courseCode: courseCode });
+});
+
 export default router;
