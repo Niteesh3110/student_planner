e6ef1419cdcee6829bdccb9aad4db28bd29087d2
Sumant Warhekar <sumantwarhekar@egmail.com>
2024-12-02 17:56:53 -0500

    Initial commit for calendar feature

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import academicPlannerRoute from "./academic_planner.js";
 import homeRoutes from "./home_page_routes.js";
 import qnaRoutes from "./qna.js";
+import calendarRoutes from "./calendar.js"
 
 const constructorMethod = (app) => {
   app.use("/", homeRoutes);
   app.use("/ap", academicPlannerRoute);
   app.use("/qna", qnaRoutes);
+  app.use("/calendar", calendarRoutes);
 
   app.use("*", (req, res) => {
     return res.status(404).json({ error: "Not Found!!!" });
