abbaa5431e33388bc97f4b544e402814ae0db049
Andrew Findley <aecf42@gmail.com>
2024-12-10 15:49:54 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ import academicPlannerRoute from "./academic_planner.js";
 import homeRoutes from "./home_page_routes.js";
 import qnaRoutes from "./qna.js";
 import calendarRoutes from "./calendar.js"
+import fileConversion from "./file_conversion.js"
 
 const constructorMethod = (app) => {
   app.use("/", homeRoutes);
   app.use("/ap", academicPlannerRoute);
   app.use("/qna", qnaRoutes);
   app.use("/calendar", calendarRoutes);
+  app.use("/file_conversion", fileConversion);
 
   app.use("*", (req, res) => {
     return res.status(404).json({ error: "Not Found!!!" });
