2a889aa1a1c6d43c4f9c67b7fef8ad26b4bc7be9
NITS <nits@Nits-Air.lan>
2024-12-15 15:29:22 -0500

    Updated package.json

--- routes/proofreading.js
+++ routes/proofreading.js
@@ -6,56 +6,52 @@ let holder = "";
 
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
-  return res.status(200).render("proofreading");
-}).post(async (req, res) => {
-    
+router
+  .route("/")
+  .get(async (req, res) => {
+    return res.status(200).render("proofreading");
+  })
+  .post(async (req, res) => {
     const theBody = req.body.givenText;
     const theFile = req.files;
     let fileGiven;
     let errors = [];
 
-    if (!theFile && !theBody){
-      errors.push("Error: You must input text or a text file!")
-      res.status(400).render('proofreading', {hasErrors: true, errors: errors});
-    }
-
-    else{
-      
+    if (!theFile && !theBody) {
+      errors.push("Error: You must input text or a text file!");
+      res
+        .status(400)
+        .render("proofreading", { hasErrors: true, errors: errors });
+    } else {
       if (!theBody) {
-        fileGiven = req.files.userFile        
-      }
-
-      else{
-
-        if (doneBefore && holder == req.body.givenText){
+        fileGiven = req.files.userFile;
+      } else {
+        if (doneBefore && holder == req.body.givenText) {
           doneBefore = false;
           holder = null;
           return res.status(200).render("proofreading");
-        }
-  
-        else {
+        } else {
           doneBefore = true;
           holder = theBody;
         }
       }
 
-      const genAI = new GoogleGenerativeAI("AIzaSyAMDWbHKmIoBxEZr_atsDdp-zVbQhSKAEQ");
+      const genAI = new GoogleGenerativeAI(
+        "AIzaSyAMDWbHKmIoBxEZr_atsDdp-zVbQhSKAEQ"
+      );
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
       let prompt;
 
-      if (fileGiven) prompt = "Proofread and Grammar Check this text: " + fileGiven.data;
-
+      if (fileGiven)
+        prompt = "Proofread and Grammar Check this text: " + fileGiven.data;
       else prompt = "Proofread and Grammar Check this text: " + theBody;
 
       const result = await model.generateContent(prompt);
 
       let finalResult = result.response.text();
 
-      return res.status(200).render('proofreading', {results: finalResult});
+      return res.status(200).render("proofreading", { results: finalResult });
     }
-    
-});
-
+  });
 
 export default router;
