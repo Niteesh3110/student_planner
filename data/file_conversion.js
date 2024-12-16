e348476ace791235f360d57490d542ee25c4a435
Andrew Findley <aecf42@gmail.com>
2024-12-16 18:37:09 -0500

    Formatted

--- data/file_conversion.js
+++ data/file_conversion.js
@@ -4,15 +4,15 @@ const convertapi = new ConvertAPI(process.env.CONVERT_API);
 export { listOptions, startConversion, clearDir };
 
 const conversionOptions = [
-    { "csv": ["pdf", "xlsx"] },
-    { "docx": ["jpg", "pdf", "png", "tiff", "txt", "webp", "xml"] },
-    { "jpg": ["pdf", "png", "svg", "tiff", "webp"] },
-    { "pdf": ["docx", "pptx", "xlsx"] },
-    { "png": ["jpg", "pdf", "svg", "tiff", "webp"] },
-    { "pptx": ["jpg", "pdf", "png", "tiff", "webp"] },
-    { "txt": ["jpg"] },
-    { "xlsx": ["csv", "jpg", "pdf", "png", "tiff", "webp"] },
-    { "xml": ["docx"] }
+    { csv: ["pdf", "xlsx"] },
+    { docx: ["jpg", "pdf", "png", "tiff", "txt", "webp", "xml"] },
+    { jpg: ["pdf", "png", "svg", "tiff", "webp"] },
+    { pdf: ["docx", "pptx", "xlsx"] },
+    { png: ["jpg", "pdf", "svg", "tiff", "webp"] },
+    { pptx: ["jpg", "pdf", "png", "tiff", "webp"] },
+    { txt: ["jpg"] },
+    { xlsx: ["csv", "jpg", "pdf", "png", "tiff", "webp"] },
+    { xml: ["docx"] },
 ];
 
 const listOptions = async (fileGiven) => {
@@ -27,29 +27,31 @@ const listOptions = async (fileGiven) => {
 };
 
 const startConversion = async (filePath, convertType) => {
-  try {
-    const result = await convertapi.convert(convertType, { File: filePath });
-    const link = result.file.url;
-    return link;
-  } catch (e) {
-    console.error(e.toString());
-  }
+    try {
+        const result = await convertapi.convert(convertType, {
+            File: filePath,
+        });
+        const link = result.file.url;
+        return link;
+    } catch (e) {
+        console.error(e.toString());
+    }
 };
 
 const clearDir = async () => {
-  fs.readdir("./public/toConvert", (err, files) => {
-    if (err) {
-      console.error(err.toString());
-      return;
-    }
-    for (let file of files) {
-      if (file === ".keep") continue;
-      fs.unlink(`./public/toConvert/${file}`, (err) => {
+    fs.readdir("./public/toConvert", (err, files) => {
         if (err) {
-          console.error(err.toString());
-          return;
+            console.error(err.toString());
+            return;
         }
-      });
-    }
-  });
+        for (let file of files) {
+            if (file === ".keep") continue;
+            fs.unlink(`./public/toConvert/${file}`, (err) => {
+                if (err) {
+                    console.error(err.toString());
+                    return;
+                }
+            });
+        }
+    });
 };
